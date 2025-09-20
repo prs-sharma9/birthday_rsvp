56ad6066b4517246a880f481647d804db479feb6
prashsharma88 <132094210+prs-sharma9@users.noreply.github.com>
2025-09-19 21:00:08 -0600

    removed testing code

--- src/components/Invite.jsx
+++ src/components/Invite.jsx
@@ -27,21 +27,20 @@ function Invite() {
 
     useEffect(() => {
         const os = checkIsApple()
-        alert(`is apple : ${os}`)
         setIsApple(os);
 
         
     }, [])
 
     function handleMapRequest() {
-        let map_url = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5007.342770967677!2d-114.20518152351222!3d51.132969221730626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537168e28971408f%3A0xa4182a92c9e48672!2s12%20Arbour%20Lake%20Dr%20NW%2C%20Calgary%2C%20AB%20T3G%204A3!5e0!3m2!1sen!2sca!4v1758332257211!5m2!1sen!2sca'
+        let map_url = 'https://maps.app.goo.gl/P7DiVDm12CsYQP2k8'
         if(isApple) {
             map_url = 'https://maps.apple.com/place?address=12+Arbour+Lake+Dr+NW%2C+Calgary+AB+T3G+4A3%2C+Canada&coordinate=51.1331424%2C-114.2025665&name=12+Arbour+Lake+Dr+NW'
         }
 
         window.open(map_url, '_blank').focus();
     }
-    
+
     return (
         <section>
             <div id="theme_image">
--- src/const.js
+++ src/const.js
@@ -1,11 +1,2 @@
-import data from "../public/event_details.json" with {type: 'json'};
 
-const event_details = data.event_detail;
-
-export const kid_name = event_details.name;
-export const event_date = event_details.date;
-export const event_month = event_details.month;
-export const event_address = event_details.address;
-export const event_time = event_details.time;
-export const event_day = event_details.day;
 export const emailjs_pub_key = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
